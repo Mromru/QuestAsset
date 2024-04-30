6f8b8f57e9314ffa012b6da934e45bcce503c0b6
Tomasz Piekarczyk <t.piekarczyk97@gmail.com>
2024-04-30 08:43:07 +0200

    Added plugin modules

---
+++ Source/QuestAssetPluginEditor/Private/QuestAssetPluginEditor.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "QuestAssetPluginEditor.h"
+
+#define LOCTEXT_NAMESPACE "FQuestAssetPluginEditorModule"
+
+void FQuestAssetPluginEditorModule::StartupModule()
+{
+	
+}
+
+void FQuestAssetPluginEditorModule::ShutdownModule()
+{
+
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FQuestAssetPluginEditorModule, QuestAssetPlugin)
